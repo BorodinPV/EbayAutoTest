fc1fb1c1770734dc94442c2c4d21d78fcb0c4d00
Pavel <inadequte@gmail.com>
2018-02-03 20:00:56 +0300

    commit

--- src/test/java/EbayAutoTest/TestEbayClass.java
+++ src/test/java/EbayAutoTest/TestEbayClass.java
@@ -16,10 +16,12 @@ import ru.yandex.qatools.allure.annotations.Step;
 import ru.yandex.qatools.allure.annotations.Title;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
+
 @Title("Автотест на Ebay")
-@Description("In this cool suite we will test only cool features")
+@Description("Тестовый сценарий")
 public class TestEbayClass {
 
     private static WebDriver driver;
@@ -35,79 +37,100 @@ public class TestEbayClass {
     }
 
     @Test
+    public void runningTheScript(){
+        userToRegister();
+        searchElement();
+        scroll("/*//*[@id=\"h2_rtm_csa_391\"]");
+        amountOfElements();
+        scroll("/*//*[@id=\"gh-ac\"]");
+        exitAccounting();
+        ExitCheck();
+    }
+
+    @Step("Регистрация нового пользователя")
     public void userToRegister() {
-
-
         driverXpathElement("//*[@id=\"gh-ug-flex\"]/a").click();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         System.out.println("Page title is: " + driver.getTitle());
-
         driverIdElement("firstname").sendKeys("Test");
-
         driverIdElement("lastname").sendKeys("Test");
-
-        driverIdElement("email").sendKeys("ebayautotest25@rambler.ru");
-
+        driverIdElement("email").sendKeys(getGenerate() + "@rambler.ru");
         driverIdElement("PASSWORD").sendKeys("rwerer5w45GHSG");
-
-
         driverWait().until(ExpectedConditions.elementToBeClickable(By.id("ppaFormSbtBtn")));
         driverIdElement("ppaFormSbtBtn").click();
+    }
 
+    @Step("Поиск элементов на Ebay")
+    public void searchElement(){
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driverIdElement("gh-ac").sendKeys("blackberry");
         driverIdElement("gh-btn").click();
+    }
 
-        scroll("/*//*[@id=\"h2_rtm_csa_391\"]");
+    @Step("Проверяем колличестов отображаемых элементов")
+    public void amountOfElements(){
         driverWait().until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("/*//*[@id=\"ListViewInner\"]/li[@class=\"sresult lvresult clearfix li shic\"]")));
-
         System.out.println("Размер Массива " + listElement("/*//*[@id=\"ListViewInner\"]/li[contains(text(), sresult)]").size());
-        scroll("/*//*[@id=\"gh-ac\"]");
+    }
 
+    @Step("Выход из учетной записи")
+    public void exitAccounting(){
         driverWait().until(ExpectedConditions.elementToBeClickable(By.id("gh-ug")));
         Actions actions = new Actions(driver);
         actions.moveToElement(driverIdElement("gh-ug")).click().build().perform();
         actions.moveToElement(driverXpathElement("//div[@id=\"gh-eb-u-o\"]/ul/li[@id=\"gh-uo\"]/a")).click().build().perform();
+    }
 
+    @Step("Проверка успешности выхода из учетной записи")
+    public void ExitCheck(){
         String exitText = driverXpathElement("/*//*[@id=\"AreaTitle\"]/div/table/tbody/tr/td/div/div[2]/div/h1/span").getText();
         if (exitText.equals("Выход успешно выполнен. До скорой встречи.")) {
             System.out.println(exitText);
+            log("Выход успешно выполнен");
         } else {
             System.out.println("Ошибка при выходе из профиля");
+            log("Ошибка при выходе из профиля");
         }
     }
-    @Step("Подсчет колличества элементов в списке")
+
+    @Step("{0}")
+    private void log(String value) {
+
+    }
+
+
     public List listElement(String xpath){
         List<WebElement> list = driver.findElements(By.xpath(xpath));
+        log("Колличество эллементов в списке: " + list.size());
         return list;
     }
 
-    //Поиск элемента по Xpath
-    @Step("Переход по Xpath")
     public WebElement driverXpathElement(String xpath) {
-
+        log("Переход по Xpath");
         return driver.findElement(By.xpath(xpath));
     }
 
-    //Поиск элемента по Id
-    @Step("Переход по Id")
     public WebElement driverIdElement(String id) {
-
+        log("Переход по Id " + id);
         return driver.findElement(By.id(id));
     }
 
-    //Скролл для прогрузки всех элементов
-    @Step("Скролл элементов")
     public void scroll(String xpath) {
+        log("Скролл cтранички");
         WebElement element = driver.findElement(By.xpath(xpath));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
     }
 
-    //Явное ожидание
-    @Step("Ожидание элемента")
     public WebDriverWait driverWait() {
+        log("Ожидание элемента");
         WebDriverWait wait = (new WebDriverWait(driver, 10));
         return wait;
+    }
+
+    public String getGenerate() {
+        UUID email = UUID.randomUUID();
+        log("Сгенерирован новый email");
+        return email.toString().substring(0,6);
     }
 
     @AfterClass
